a738f9b2c493cb84875dc75b6a4a5307fa68362f
kangwenjun <603164555@qq.com>
2020-04-16 08:20:28 +0800

    instanceof

---
+++ Operator.java
@@ -0,0 +1,9 @@
+
+public class Operator {
+	public static void main(String[] args) {
+		//System.out.println(sizeof(args)); // cannot find symbol sizeof
+		String str = new String();
+		boolean bRet = str instanceof String;
+		System.out.println(bRet);
+	}
+}
